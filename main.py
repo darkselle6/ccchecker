de5a2ffaf46411fc6fea9215f5feb4f7806c76a0
darkselle6 <103187014+darkselle6@users.noreply.github.com>
2022-04-11 22:39:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import asyncio
 import logging
 logging.basicConfig(level=logging.DEBUG, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
-bot = AsyncTeleBot('5142948808:AAGO4SQp7MfDOI2vzsxhpPp2_e6NWLyOcUQ')
+bot = AsyncTeleBot('5245288319:AAGuNTSMmyY_1geLYyXO7bfmlO1ye41Vass')
 
-admins = [5197853005]
+admins = [1655261524]
 file = open('groups.txt','r')
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
@@ -16,6 +16,6 @@ async def send_welcome(message):
     first = await bot.reply_to(message, """\
 Starting Bot\
 """)
-    await bot.edit_message_text("Heya How are you ??\nWant to Check ccs?? Send /cmds for more",message.chat.id,first.id,parse_mode="HTML")
+    await bot.edit_message_text("Heya How are you ??\nWant to Check ccs?? Send /cmds for more bot by @shadowdemin",message.chat.id,first.id,parse_mode="HTML")
 
 #asyncio.run(bot.polling())
